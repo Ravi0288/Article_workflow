e4ca10ce8c883aaa081c08479b54447de84fcdce
Prasenjit Kushwaha <pk15pi@gmail.com>
2024-03-08 21:08:11 +0530

    bug fixes

--- actions/ftp.py
+++ actions/ftp.py
@@ -10,28 +10,29 @@ import time
 
 
 def connect_with_ftp(request):
-    try:
-        # Connect FTP Server
-        connect = ftplib.FTP(settings.HOSTNAME, settings.USERNAME, settings.PASSWORD)
+    pass
+    # try:
+    #     # Connect FTP Server
+    #     connect = ftplib.FTP(settings.HOSTNAME, settings.USERNAME, settings.PASSWORD)
 
-        # force UTF-8 encoding
-        connect.encoding = "utf-8"
+    #     # force UTF-8 encoding
+    #     connect.encoding = "utf-8"
 
-        # Enter File Name with Extension
-        filename = "test.txt"
-        # connect.cwd('/home/admin')
+    #     # Enter File Name with Extension
+    #     filename = "test.txt"
+    #     # connect.cwd('/home/admin')
 
-        # Write file in binary mode
-        with open(filename, "wb") as file:
-            # Command for Downloading the file
-            connect.retrbinary(f"RETR {filename}", file.write)
+    #     # Write file in binary mode
+    #     with open(filename, "wb") as file:
+    #         # Command for Downloading the file
+    #         connect.retrbinary(f"RETR {filename}", file.write)
             
-        # Display the content of downloaded file
-        file = open(filename, "r")
+    #     # Display the content of downloaded file
+    #     file = open(filename, "r")
         
-        # Close the Connection
-        connect.quit()
-        return Response("File pulled from FTP successfullt")
+    #     # Close the Connection
+    #     connect.quit()
+    #     return Response("File pulled from FTP successfullt")
     
-    except Exception as e:
-        return Response(f'''Error occured while pulling the content from the FTP. Error : {e}''')
+    # except Exception as e:
+    #     return Response(f'''Error occured while pulling the content from the FTP. Error : {e}''')
