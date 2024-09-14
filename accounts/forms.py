c94eff3d3f773c55408366c74c09f40074a287d3
pk15pi <76875206+pk15pi@users.noreply.github.com>
2024-09-14 17:14:12 +0530

    bug fixes

--- accounts/forms.py
+++ accounts/forms.py
@@ -5,13 +5,14 @@ from django.contrib.auth.forms import AuthenticationForm
 
 class UserForm(forms.ModelForm):
     # groups = forms.ModelMultipleChoiceField(queryset=Group.objects.all(), widget=forms.CheckboxSelectMultiple)
-    groups = forms.ModelChoiceField(queryset=Group.objects.all(), widget=forms.Select)
+    # groups = forms.ModelChoiceField(queryset=Group.objects.all(), widget=forms.Select)
 
     class Meta:
         model = User
         fields = ['username', 'email', 'password', 'groups']
         widgets = {
             'password': forms.PasswordInput(),
+            # 'groups': forms.Select(attrs={'class': 'group-width'}),
         }
 
 class GroupForm(forms.ModelForm):
