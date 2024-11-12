9637e0e01b6453693539231f1f78d33c94e4eb34
pk15pi <76875206+pk15pi@users.noreply.github.com>
2024-11-12 20:13:16 +0530

    reports added

--- reports/urls.py
+++ reports/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('provider-access-view/', views.Provider_access_view, name='provider-access-report'),
-    path('backlog-view/', views.backlog_view, name='backlog-report'),
+    path('provider-access-report/', views.Provider_access_view, name='provider-access-report'),
+    path('backlog-report/', views.backlog_view, name='backlog-report'),
 ]
