a5437838cad669115c0b8605c694420fb46c3d9b
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-02-11 23:10:30 +0530

    dsaf

--- step4/urls.py
+++ step4/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .journal import Journal_viewset
+from .migrate_to_step_4 import migrate_to_step4
 
 router = DefaultRouter()
 router.register('journal', Journal_viewset, basename='journal')
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('migrate-to-step-3/', migrate_to_step4, name="migrate-to-step-3"),
 ]
