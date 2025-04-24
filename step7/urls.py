d831efb01bb7f64db40feaba7ab3db0870a8d15a
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-04-24 19:20:25 +0530

    test file added

--- step7/urls.py
+++ step7/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
 from .migrate_to_step_7 import migrate_to_step7
+from .test import test
 
 urlpatterns = [
     # path('', include(router.urls)),
     path('migrate-to-step-7/', migrate_to_step7, name="migrate-to-step-7"),
+    path('test/', migrate_to_step7, name="migrate-to-step-7"),
 ]
