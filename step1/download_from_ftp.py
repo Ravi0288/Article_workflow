420889179ca27656be73018ccd9ff8b5e7fb91d6
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-05-29 21:44:31 +0530

    pull from sftp: update code commented

--- step1/download_from_ftp.py
+++ step1/download_from_ftp.py
@@ -81,15 +81,15 @@ def download_file(ftp_connection, article, item):
         return
     
     # if file exists than check the file size. If file size is different update the existing record
-    if (x.exists() and not (x[0].file_size == file_size)):
-        content = BytesIO()
-        ftp_connection.retrbinary(f'RETR {article}', content.write)
-        content.seek(0)
-        x[0].status = 'waiting'
-        x[0].is_content_changed = True
-        article = str(x[0].id) + '.' + article.split('.')[-1]
-        x[0].file_content.save(article, content)
-        return
+    # if (x.exists() and not (x[0].file_size == file_size)):
+    #     content = BytesIO()
+    #     ftp_connection.retrbinary(f'RETR {article}', content.write)
+    #     content.seek(0)
+    #     x[0].status = 'waiting'
+    #     x[0].is_content_changed = True
+    #     article = str(x[0].id) + '.' + article.split('.')[-1]
+    #     x[0].file_content.save(article, content)
+    #     return
     
 
 # function to download folder with its content and convert to zip, finally save to table
@@ -136,13 +136,12 @@ def download_folder_from_ftp_and_save_zip(ftp_connection, article, item):
             return
 
         # if file exists than check the file size. If file size is different update the existing record
-        if (x.exists() and not (x[0].file_size == os.path.getsize(zipped_file))):
-            x[0].status = 'active'
-            with open(zipped_file, 'rb') as zip_file:
-                zip_file.seek(0)
-                x[0].file_content.save(article, zip_file)
+        # if (x.exists() and not (x[0].file_size == os.path.getsize(zipped_file))):
+        #     x[0].status = 'active'
+        #     with open(zipped_file, 'rb') as zip_file:
+        #         zip_file.seek(0)
+        #         x[0].file_content.save(article, zip_file)
 
-    print("zipped and exiting content")
             
     # Cleanup temporary directory
     shutil.rmtree(temp_dir)
