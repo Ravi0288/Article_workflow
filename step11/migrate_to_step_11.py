33c332126dc240ba8d7af0550b88869652528df6
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-06-10 20:35:57 +0530

    fetch secret key added

--- step11/migrate_to_step_11.py
+++ step11/migrate_to_step_11.py
@@ -110,11 +110,18 @@ def migrate_to_step11(request):
     if not articles.count() :
         return render(request, 'common/dashboard.html', context=context)
 
+    # get_aws_credentials function will return key1 and key2 if sucess, else False and error message.
+    key1, key2 = get_aws_credentials()
+    if not key1:
+        context['message'] = key2
+        return render(request, 'common/dashboard.html', context=context)        
+    
+    
     stagin_info = {
         'base_s3_uri' : settings.BASE_S3_URI,
         's3_uris' : settings.S3_URIS,
-        'aws_access_key' : settings.AWS_S3_ACCESS_KEY,
-        'aws_secret_key' : settings.AWS_S3_SECRET_KEY,
+        'aws_access_key' : key1,
+        'aws_secret_key' : key2,
         'bucket' : settings.S3_BUCKET,
         'prefix' : settings.S3_PREFIX,
         'base_path' : os.path.join(settings.BASE_DIR, settings.ALMA_STAGING)
