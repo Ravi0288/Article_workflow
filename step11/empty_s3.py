bd6849d42b543b220d62320b81741ae8bbca158f
pk15pi <76875206+pk15pi@users.noreply.github.com>
2025-06-12 19:56:26 +0530

    bug fixes

--- step11/empty_s3.py
+++ step11/empty_s3.py
@@ -15,7 +15,7 @@ def empty_s3_bucket(request):
         'heading' : 'Message',
         'message' : f'''S3 Bucket emptied successfully. '''
     }
-    
+
     # get_aws_credentials function will return key1 and key2 if sucess, else False and error message.
     key1, key2 = get_aws_credentials()
     if not key1:
@@ -25,8 +25,8 @@ def empty_s3_bucket(request):
     stagin_info = {
         'base_s3_uri' : settings.BASE_S3_URI,
         's3_uris' : settings.S3_URIS,
-        'aws_access_key' : settings.AWS_S3_ACCESS_KEY,
-        'aws_secret_key' : settings.AWS_S3_SECRET_KEY,
+        'aws_access_key' : key1,
+        'aws_secret_key' : key2,
         'bucket' : settings.S3_BUCKET,
         'prefix' : settings.S3_PREFIX,
         'base_path' : os.path.join(settings.BASE_DIR, settings.ALMA_STAGING)
